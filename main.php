073ffa489e586fdc36f19de8f0b32b4efd0da027
Mikhail Kucherenko <makucherenko@list.ru>
2025-05-03 01:41:58 +0300

    Remove loading wrong icons

--- main.php
+++ main.php
@@ -21,8 +21,6 @@ $showIcon = tpl_getConf('showIcon');
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-		<link rel="icon" type="image/png" href="assets/img/favicon.png">
 		<title>
 			<?php tpl_pagetitle()?> | <?php echo strip_tags($conf['title']) ?>
 		</title>
